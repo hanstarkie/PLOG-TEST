6dbbd583b7728bb46785aece11da61e4f65cd80f
hanstarkie <33317594+hanstarkie@users.noreply.github.com>
2018-02-09 13:54:13 +0000

    Add files via upload

---
+++ PLOGGING /js/index.js
@@ -0,0 +1,14 @@
+//Wow JS
+new WOW().init();
+
+// Slick Slider carousel
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true
+  });
+
+  $('.caption_slides').slick({
+    dots: true
+  });
+
+});
